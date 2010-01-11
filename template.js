0e9917baeac6dfab8aeb67b978bc64d7a44aac43
Greg Ritter <gjritter@codespin.com>
2010-01-10 22:52:27 -0700

    Replaced use of wait() with an asynchronous callback.

--- template.js
+++ template.js
@@ -13,36 +13,28 @@ var posix = require("posix");
 
 var cache = {};
   
-var tmpl  = function (str, data){
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/[\t\r\n% ]/.test(str) ?
-    cache[str] = cache[str] ||
-    // read the template from the file specified
-    tmpl(posix.cat(str).wait()) :
-    
-    // Generate a reusable function that will serve as a template
-    // generator (and which will be cached).
-    new Function("obj",
-		 "var p=[],print=function(){p.push.apply(p,arguments);};" +
-		 
-		 // Introduce the data as local variables using with(){}
-		 "with(obj){p.push('" +
-		 
-		 // Convert the template into pure JavaScript
-		 str
-		 .replace(/[\r\n\t]/g, " ")
-		 .split("<%").join("\t")
-		 .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                 .replace(/\t=(.*?)%>/g, "',$1,'")
-                 .split("\t").join("');")
-                 .split("%>").join("p.push('")
-                 .split("\r").join("\\'")
-                 + "');}return p.join('');");
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-};
+var create = function(file_name, callback) {
+	posix.cat(file_name).addCallback(function(file_contents) {
+	    callback(new Function("obj",
+			 "var p=[],print=function(){p.push.apply(p,arguments);};" +
 
+			 // Introduce the data as local variables using with(){}
+			 "with(obj){p.push('" +
+
+			 // Convert the template into pure JavaScript
+			 file_contents
+			 .replace(/[\r\n\t]/g, " ")
+			 .split("<%").join("\t")
+			 .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+	                 .replace(/\t=(.*?)%>/g, "',$1,'")
+	                 .split("\t").join("');")
+	                 .split("%>").join("p.push('")
+	                 .split("\r").join("\\'")
+	                 + "');}return p.join('');"));
+	}).addErrback(function(e) {
+		// do something to handle the error
+	});
+}
 
 /* exports */
-exports.tmpl = tmpl;
+exports.create = create;
--- test.js
+++ test.js
@@ -1,24 +1,44 @@
 (function() {
 	var sys = require('sys'),
 		test = require('mjsunit'),
-		template = require('./template');
+		template = require('./template'),
+		pending_callbacks = 0;
+
+	function expect_callback() {
+		pending_callbacks += 1;
+	}
+
+	function receive_callback() {
+		pending_callbacks -= 1;
+	}
+
 
 	var myobj = {obj : {num: 5, person:'Jack'}};
 	var myobj2 = {obj : {num: 10, person:'Jill'}};
 
-	var template_value = template.tmpl('./tmpls/t1.template', myobj);
-	test.assertEquals('5 apples for Jack ', template_value);
+	expect_callback();
+	template.create('./tmpls/t1.template', function(my_template_function) {
+		receive_callback();
+		test.assertEquals('5 apples for Jack ', my_template_function(myobj));
+	});
 
-	var name = 'chad';
-	template_value = template.tmpl('./tmpls/t2.template', {name:name});
-	test.assertEquals('chad', template_value);
-
-	template_value = template.tmpl('./tmpls/t2.template', {name:'Bob'});
-	test.assertEquals('Bob', template_value);
-
-	var f = template.tmpl('./tmpls/t2.template');
-	test.assertEquals('alice', f({name:'alice'}));
-
-	var data = {users: [{name:'Chad', age:25}, {name:'Bob', age:40}]};
-	test.assertEquals('Chad is 25 years old.\nBob is 40 years old.\n', template.tmpl('./tmpls/t3.template', data));
+	expect_callback();
+	template.create('./tmpls/t2.template', function(my_template_function) {
+		receive_callback();
+		test.assertEquals('Bob', my_template_function({name:'Bob'}));
+	});
+	
+	expect_callback();
+	template.create('./tmpls/t3.template', function(my_template_function) {
+		var data = {users: [{name:'Chad', age:25}, {name:'Bob', age:40}]};
+		receive_callback();
+		test.assertEquals('Chad is 25 years old.\nBob is 40 years old.\n', my_template_function(data));
+	});
+	
+	// assert that all callbacks were called within the alloted time and exit
+	
+	setTimeout(function () {
+		test.assertEquals(0, pending_callbacks);
+		process.exit();
+	}, 50);
 }());
